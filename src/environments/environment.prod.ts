348b403190e8ff7ad12ca0951e8d8da2276610fb
ansumanparija <ansumanparija@users.noreply.github.com>
2020-07-18 08:14:51 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+   CLOUDANT_USERNAME: "0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix",
+  CLOUDANT_PASSWORD:
+    "fcdd6887f247992f642515dd5b1cb6b166ef21d802d5b9bc9eb45f88a3d41f38",
+  CLOUDANT_URL:
+    "https://0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix:fcdd6887f247992f642515dd5b1cb6b166ef21d802d5b9bc9eb45f88a3d41f38@0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix.cloudantnosqldb.appdomain.cloud",
+  BLASTER_DB: "blaster_db",
+  INSTITUTE_DB: "institute_db"
 };
