7fe46c5b28b9c78f4ffc2f828acbe78cfface61f
ansumanparija <ansumanparija@users.noreply.github.com>
2020-07-18 17:53:37 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  CLOUDANT_USERNAME: "0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix",
+   CLOUDANT_USERNAME: "0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix",
   CLOUDANT_PASSWORD:
     "fcdd6887f247992f642515dd5b1cb6b166ef21d802d5b9bc9eb45f88a3d41f38",
   CLOUDANT_URL:
     "https://0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix:fcdd6887f247992f642515dd5b1cb6b166ef21d802d5b9bc9eb45f88a3d41f38@0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix.cloudantnosqldb.appdomain.cloud",
   BLASTER_DB: "blaster_db",
-  INSTITUTE_DB: "institute_db",
-   BLOCKCHAIN_DB: "blockchain_db",
+  INSTITUTE_DB: "institute_db"
+  BLOCKCHAIN_DB: "blockchain_db",
   INST_ID: "I1234",
   INST_NAME: "Apollo Hospital"
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
