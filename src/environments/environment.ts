8019302f1a69c28a1d0d9a446111e14c3a37b565
ansumanparija <ansumanparija@users.noreply.github.com>
2020-07-18 18:03:01 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   CLOUDANT_URL:
     "https://0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix:fcdd6887f247992f642515dd5b1cb6b166ef21d802d5b9bc9eb45f88a3d41f38@0376e4ae-e8d8-4313-834a-ae9cfad7710f-bluemix.cloudantnosqldb.appdomain.cloud",
   BLASTER_DB: "blaster_db",
-  INSTITUTE_DB: "institute_db"
+  INSTITUTE_DB: "institute_db",
   BLOCKCHAIN_DB: "blockchain_db",
   INST_ID: "I1234",
   INST_NAME: "Apollo Hospital",
